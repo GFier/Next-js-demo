d7269dbde234869a85db0ae6ad82be87ad75ab46
GFIer <guidoarg@gmail.com>
2021-03-03 23:28:59 -0300

    on keys

--- keys/keys.js
+++ keys/keys.js
@@ -3,9 +3,11 @@
 // exchange api => https://www.exchangerate-api.com/
 // google maps api => https://cloud.google.com/maps-platform?hl=es
 
-export const Keys={
+const Keys={
   ip_api_key: 'your_ip_apikey',
   weather_api_key: 'your_weather_api_key',
   exchange_api_key: 'your_exchange_api_key',
   google_maps_key: 'your_google_maps_api_key'
 }
+
+export default Keys;
